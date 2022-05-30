76621668081d81d45a93aa2ffe65a77b43a7d605
Davi <davistalleiken@gmail.com>
2022-05-29 21:03:20 -0300

    exercícios JS realizados

---
+++ JavaScript/variaveis e tipos/substituir-numeros/script.js
@@ -0,0 +1,27 @@
+/*
+function substituirNumerosPares(array) {
+    if(!array.length) return -1;
+
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] === 0) {
+            console.log("Você é zero.")
+        } else if (array[i] % 2 === 0) {
+            console.log(`Substituindo ${array[i]} por 0...`)
+            array[i] = 0;
+        }
+    }
+
+    return array;
+}
+
+let arr = [1, 3, 4, 6, 80, 33, 23, 90];
+console.log(substituirNumerosPares([]));
+*/
+
+const meuObjeto = {
+    chave: 12,
+    chave1: 66
+}
+
+console.log(meuObjeto.chave);
+console.log(meuObjeto[chave]);
